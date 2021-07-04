eaa8d1fbb1913d3bc9647447febee76c3350f696
gasseklopper <m-morley@gmx.de>
2021-07-04 19:10:56 +0200

    added tag cloud

--- .eleventy.js
+++ .eleventy.js
@@ -27,6 +27,38 @@ module.exports = function (eleventyConfig) {
 	// https://www.11ty.dev/docs/data-deep-merge/
 	eleventyConfig.setDataDeepMerge(true);
 
+
+	// Get the first `n` elements of a collection.
+	eleventyConfig.addFilter("head", (array, n) => {
+		if (n < 0) {
+			return array.slice(n);
+		}
+
+		return array.slice(0, n);
+	});
+
+	// Return the smallest number argument
+	eleventyConfig.addFilter("min", (...numbers) => {
+		return Math.min.apply(null, numbers);
+	});
+
+	function filterTagList(tags) {
+		return (tags || []).filter(tag => ["all", "nav", "post", "posts"].indexOf(tag) === -1);
+	}
+
+	eleventyConfig.addFilter("filterTagList", filterTagList)
+
+	// Create an array of all tags
+	eleventyConfig.addCollection("tagList", function (collection) {
+		let tagSet = new Set();
+		collection.getAll().forEach(item => {
+			(item.data.tags || []).forEach(tag => tagSet.add(tag));
+		});
+
+		return filterTagList([...tagSet]);
+	});
+
+
 	/* Markdown Plugins */
 	let markdownIt = require("markdown-it");
 	let markdownItAnchor = require("markdown-it-anchor");

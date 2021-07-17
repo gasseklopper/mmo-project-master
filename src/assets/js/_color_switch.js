a7b9c271dd505e1dbd731269e5149f6511a25d96
gasseklopper <m-morley@gmx.de>
2021-07-17 21:29:49 +0200

    added color switch

--- src/assets/js/_color_switch.js
+++ src/assets/js/_color_switch.js
@@ -2,18 +2,14 @@ let colorswitch = document
 	.querySelectorAll(".themeingbutton")
 	.forEach((elem) => {
 		elem.onclick = (e) => {
-			console.log("click switch");
-			const doc = document.firstElementChild;
-			let themeWrapper = document.querySelector(".theme-wrapper");
-			if (themeWrapper.classList.contains("theme-dark")) {
-				themeWrapper.classList.remove("theme-dark");
-				themeWrapper.classList.add("theme-light");
-				console.log("click switch light");
+			var doc = document.firstElementChild;
+			if (doc.getAttribute("color-scheme") == "dark") {
+				doc.setAttribute("color-scheme", "light");
+			} else if (doc.getAttribute("color-scheme") == "light") {
+				doc.setAttribute("color-scheme", "dark");
+			} else if (doc.getAttribute("color-scheme") == "default") {
 				doc.setAttribute("color-scheme", "light");
 			} else {
-				themeWrapper.classList.remove("theme-light");
-				themeWrapper.classList.add("theme-dark");
-				console.log("click switch dark");
 				doc.setAttribute("color-scheme", "dark");
 			}
 		};

f26b069ae65d80d00ee00f968df7827976b94dea
gasseklopper <m-morley@gmx.de>
2021-08-08 20:51:14 +0200

    updated color selction in local storage

--- src/assets/js/_color_switch.js
+++ src/assets/js/_color_switch.js
@@ -1,20 +1,8 @@
 let colorswitch = document
 	.querySelectorAll(".themeingbutton")
 	.forEach((elem) => {
-		var doc = document.body;
-		let onpageLoad = localStorage.getItem("color-scheme");
-
-		if (onpageLoad === "dark") {
-			doc.setAttribute("color-scheme", onpageLoad);
-		}
-
-		console.log("onpageLoad", onpageLoad);
-
 		elem.onclick = (e) => {
-			console.log("doc", doc);
-			let onpageLoad = localStorage.getItem("theme") || "";
-			localStorage.setItem("theme", "dark");
-
+			var doc = document.firstElementChild;
 			if (doc.getAttribute("color-scheme") == "dark") {
 				doc.setAttribute("color-scheme", "light");
 				localStorage.setItem("color-scheme", "light");
@@ -30,26 +18,3 @@ let colorswitch = document
 			}
 		};
 	});
-
-// // On page load set the theme.
-// (function() {
-// 	let onpageLoad = localStorage.getItem("theme") || "";
-// 	let element = document.body;
-// 	element.classList.add(onpageLoad);
-// 	document.getElementById("theme").textContent =
-// 	  localStorage.getItem("theme") || "light";
-//   })();
-
-//   function themeToggle() {
-// 	let element = document.body;
-// 	element.classList.toggle("dark-mode");
-
-// 	let theme = localStorage.getItem("theme");
-// 	if (theme && theme === "dark-mode") {
-// 	  localStorage.setItem("theme", "");
-// 	} else {
-// 	  localStorage.setItem("theme", "dark-mode");
-// 	}
-
-// 	document.getElementById("theme").textContent = localStorage.getItem("theme");
-//   }

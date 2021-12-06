2310c323656e03e4d9df5778129636efef66b8bf
gasseklopper <m-morley@gmx.de>
2021-12-06 12:05:18 +0100

    if footer bubbles is available

--- src/assets/js/footer/index.js
+++ src/assets/js/footer/index.js
@@ -2,110 +2,112 @@ console.log("footer");
 
 // canvasbg
 const canvasbg = document.getElementsByClassName("footer_bubbles__canvasbg")[0];
-const ctxbg = canvasbg.getContext("2d");
-
-canvasbg.width = window.innerWidth;
-canvasbg.height = window.innerHeight;
-
-// canvas
 const canvas = document.getElementsByClassName("footer_bubbles__canvas")[0];
-const ctx = canvas.getContext("2d");
 
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+if (canvasbg && canvas) {
+	const ctxbg = canvasbg.getContext("2d");
 
-let Bubbles = [];
-let bgBubbles = [];
-
-function addBubble() {
-	Bubbles.push(new Bubble("rgb(255,194,194", 2.8));
-}
-
-function addbgBubble() {
-	bgBubbles.push(new Bubble("rgb(255,255,255", 5.5));
-}
-
-class Bubble {
-	constructor(color, yspeed) {
-		this.r = Math.random() * 150 + 30;
-		this.x = Math.random() * canvas.width;
-		this.y = Math.random() * 20 + canvas.height + this.r;
-		this.life = true;
-		this.vy = yspeed;
-		this.vr = 0;
-		this.vx = Math.random() * 4 - 2;
-		this.color = color;
-	}
-
-	update() {
-		this.vy += 0.00001;
-		this.vr += 0.02;
-		this.y -= this.vy;
-		this.x += this.vx;
-		if (this.r > 1) {
-			this.r -= this.vr;
-		}
-		if (this.r <= 1) {
-			this.life = false;
-		}
-	}
-
-	draw() {
-		ctx.beginPath();
-		ctx.arc(this.x, this.y, this.r, 0, Math.PI * 2, false);
-		ctx.fillStyle = this.color;
-		ctx.fill();
-		ctx.closePath();
-	}
-}
-
-// function handleBubbles
-function handleBubbles() {
-	for (let i = Bubbles.length - 1; i >= 0; i--) {
-		Bubbles[i].update();
-		if (!Bubbles[i].life) {
-			Bubbles.splice(i, 1);
-		}
-	}
-	for (let i = bgBubbles.length - 1; i >= 0; i--) {
-		bgBubbles[i].update();
-		if (!bgBubbles[i].life) {
-			bgBubbles.splice(i, 1);
-		}
-	}
-
-	if (Bubbles.length < canvas.width / 4) {
-		addBubble();
-	}
-	if (bgBubbles.length < canvas.width / 12) {
-		addbgBubble();
-	}
-}
-
-function animate(params) {
-	ctx.clearRect(0, 0, canvas.width, canvas.height);
-	ctxbg.clearRect(0, 0, canvas.width, canvas.height);
-
-	handleBubbles();
-
-	for (let i = bgBubbles.length - 1; i >= 0; i--) {
-		bgBubbles[i].draw(ctxbg);
-	}
-
-	for (let i = Bubbles.length - 1; i >= 0; i--) {
-		Bubbles[i].draw(ctx);
-	}
-
-	requestAnimationFrame(animate);
-}
-
-window = addEventListener("load", animate);
-
-window = addEventListener("resize", () => {
 	canvasbg.width = window.innerWidth;
 	canvasbg.height = window.innerHeight;
+
+	// canvas
+	const ctx = canvas.getContext("2d");
+
 	canvas.width = window.innerWidth;
 	canvas.height = window.innerHeight;
+
 	let Bubbles = [];
 	let bgBubbles = [];
-});
+
+	function addBubble() {
+		Bubbles.push(new Bubble("rgb(255,194,194", 2.8));
+	}
+
+	function addbgBubble() {
+		bgBubbles.push(new Bubble("rgb(255,255,255", 5.5));
+	}
+
+	class Bubble {
+		constructor(color, yspeed) {
+			this.r = Math.random() * 150 + 30;
+			this.x = Math.random() * canvas.width;
+			this.y = Math.random() * 20 + canvas.height + this.r;
+			this.life = true;
+			this.vy = yspeed;
+			this.vr = 0;
+			this.vx = Math.random() * 4 - 2;
+			this.color = color;
+		}
+
+		update() {
+			this.vy += 0.00001;
+			this.vr += 0.02;
+			this.y -= this.vy;
+			this.x += this.vx;
+			if (this.r > 1) {
+				this.r -= this.vr;
+			}
+			if (this.r <= 1) {
+				this.life = false;
+			}
+		}
+
+		draw() {
+			ctx.beginPath();
+			ctx.arc(this.x, this.y, this.r, 0, Math.PI * 2, false);
+			ctx.fillStyle = this.color;
+			ctx.fill();
+			ctx.closePath();
+		}
+	}
+
+	// function handleBubbles
+	function handleBubbles() {
+		for (let i = Bubbles.length - 1; i >= 0; i--) {
+			Bubbles[i].update();
+			if (!Bubbles[i].life) {
+				Bubbles.splice(i, 1);
+			}
+		}
+		for (let i = bgBubbles.length - 1; i >= 0; i--) {
+			bgBubbles[i].update();
+			if (!bgBubbles[i].life) {
+				bgBubbles.splice(i, 1);
+			}
+		}
+
+		if (Bubbles.length < canvas.width / 4) {
+			addBubble();
+		}
+		if (bgBubbles.length < canvas.width / 12) {
+			addbgBubble();
+		}
+	}
+	window = addEventListener("load", animate);
+
+	window = addEventListener("resize", () => {
+		canvasbg.width = window.innerWidth;
+		canvasbg.height = window.innerHeight;
+		canvas.width = window.innerWidth;
+		canvas.height = window.innerHeight;
+		let Bubbles = [];
+		let bgBubbles = [];
+	});
+
+	function animate() {
+		ctx.clearRect(0, 0, canvas.width, canvas.height);
+		ctxbg.clearRect(0, 0, canvas.width, canvas.height);
+
+		handleBubbles();
+
+		for (let i = bgBubbles.length - 1; i >= 0; i--) {
+			bgBubbles[i].draw(ctxbg);
+		}
+
+		for (let i = Bubbles.length - 1; i >= 0; i--) {
+			Bubbles[i].draw(ctx);
+		}
+
+		requestAnimationFrame(animate);
+	}
+}

420305beec056ebd08791df3db2c81aa5ea49015
gasseklopper <m-morley@gmx.de>
2022-02-02 09:02:51 +0100

    add colr system s2

--- src/assets/js/_color_switch.js
+++ src/assets/js/_color_switch.js
@@ -3,25 +3,30 @@ let colorswitch = document
 	.forEach((elem) => {
 		elem.onclick = (e) => {
 			var doc = document.firstElementChild;
-			
-			if (doc.getAttribute("color-scheme") == "dark") {
-				doc.setAttribute("color-scheme", "light");
-				localStorage.setItem("color-scheme", "light");
-			} else if (doc.getAttribute("color-scheme") == "light") {
+
+			if (doc.getAttribute("color-scheme") === "dark") {
+				doc.setAttribute("color-scheme", "lights2");
+				localStorage.setItem("color-scheme", "lights2");
+			} else if (doc.getAttribute("color-scheme") === "lights2") {
 				doc.setAttribute("color-scheme", "dark");
 				localStorage.setItem("color-scheme", "dark");
-			} else if (doc.getAttribute("color-scheme") == "default") {
-				doc.setAttribute("color-scheme", "light");
-				localStorage.setItem("color-scheme", "light");
+			} else if (doc.getAttribute("color-scheme") === "default") {
+				doc.setAttribute("color-scheme", "lights2");
+				localStorage.setItem("color-scheme", "lights2");
 			} else {
-				if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-					doc.setAttribute("color-scheme", "light");
-					localStorage.setItem("color-scheme", "light");
-				} else if (window.matchMedia && window.matchMedia('(prefers-color-scheme: light)').matches) {
+				if (
+					window.matchMedia &&
+					window.matchMedia("(prefers-color-scheme: dark)").matches
+				) {
+					doc.setAttribute("color-scheme", "lights2");
+					localStorage.setItem("color-scheme", "lights2");
+				} else if (
+					window.matchMedia &&
+					window.matchMedia("(prefers-color-scheme: lights2)").matches
+				) {
 					doc.setAttribute("color-scheme", "dark");
 					localStorage.setItem("color-scheme", "dark");
 				}
-				
 			}
 		};
 	});
